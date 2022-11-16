1a820f44c94118fd8c649bcdebe2c59d7dc50e44
jcmertz <joe@joemertz.com>
2022-11-16 09:38:38 +0000

    Bug fix

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ client.on('messageCreate', message => {
     return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
   }
   try {
-    command.execute(openai, Discord, message, args);
+    command.execute(Discord, message, args);
     success = true;
   } catch (error) {
     console.error(error);
